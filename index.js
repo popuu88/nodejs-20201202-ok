5dddc9412947c47185ffb5141bc5cc6fa78ec410
andy <wumiller88@gmail.com>
2020-12-02 19:14:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const greeting = require('./src/greeting');
+
+greeting.hello()
+
+console.log('test')
+
+/* const request = require('request')
+request({ url: 'https://tw.movies.yahoo.com/movie_thisweek.html',
+json: true
+}, (error, response, body) => {
+console.log(body)  
+//console.log(JSON.stringify(body, null, 2))
+}) */
+
+
+
+
+
+/* const os = require('os')
+console.log(os.hostname()) */
+
+// console.log('hello');
+
